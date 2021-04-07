1954953e618e9ad78f538e015749ed2422597e4a
Sam Bennett <samuelhb201@gmail.com>
2021-04-07 12:33:38 -0700

    This program gives the amount of change for values less then a dollar

--- change.py
+++ change.py
@@ -1,20 +1,16 @@
 # Author: Samuel Bennett
 # Date: 4/7/2021
 # Description: Prints the amount of change and for a value less then a dollar
+print("This program gives the amount of change on a value less then a dollar")
+change=int(input("Please enter an value between 0-99: "))
 
-print("This program gives the amount of change for values less then a dollar.")
-change = int(input("Please enter amount of change 0 to 99: "))
+print("quarters:", change//25)
+change = change%25
 
-def coin(amount, name, value):
-    num_coins = 0
-    while amount >= value:
-        num_coins += 1
-        amount -= value
-    print(num_coins, name + ("s"))
-    return amount
+print("dimes:", change//10)
+change =change%10
 
+print("nickles:", change//5)
+change = change%5
 
-change = coin(change, "quarter", 25)
-change = coin(change, "dime", 10)
-change = coin(change, "nickle", 5)
-change = coin(change, "penny", 1)
+print("pennies:", change//1)
